541028e67aa1bb8cd0ed3a786fd5d4c48293e46a
Gjergj Sheldija <gjergj.sheldija@raylytic.com>
2026-02-28 18:16:04 +0100

    Add copyright notice to main.go

--- main.go
+++ main.go
@@ -1,3 +1,6 @@
+// Copyright (c) 2026 Gjergj <gjergj@sheldija.net>. All rights reserved.
+// This source code is licensed under the MIT License found in the LICENSE file in the root directory of this source tree.
+
 package main
 
 import (
